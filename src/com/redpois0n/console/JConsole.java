fc3881ea51613562b4a93f6096c07d4518862cc2
redpois0n <red@cmail.nu>
2015-02-22 10:51:06 +0100

    Blinking

--- src/com/redpois0n/console/JConsole.java
+++ src/com/redpois0n/console/JConsole.java
@@ -7,23 +7,28 @@ import javax.swing.JComponent;
 @SuppressWarnings("serial")
 public class JConsole extends JComponent {
 	
-	private int width;
-	private int height;
+	private int columns;
+	private int rows;
 	
 	private int charwidth;
 	private int charheight;
+	
+	private boolean blinking;
+	private boolean blinkCursor;
 		
 	public JConsole() {
-		this.width = 80;
-		this.height = 30;
+		this.columns = 80;
+		this.rows = 30;
 		
 		this.charwidth = 8;
 		this.charheight = 12;
+		
+		this.blinkCursor = true;
 	}
 
 	@Override
 	public void paintComponent(Graphics g) {
-		g.fillRect(0, 0, getRealX(width), getRealY(height));
+		g.fillRect(0, 0, getRealX(columns), getRealY(rows));
 	}
 	
 	public int getRealX(int x) {
@@ -33,5 +38,29 @@ public class JConsole extends JComponent {
 	public int getRealY(int y) {
 		return y * charheight;
 	}
+	
+	public void toggleBlink() {
+		blinkCursor = !blinkCursor;
+		
+		if (blinkCursor) {
+			new Thread(new BlinkRunnable()).start();
+		}
+	}
+	
+	public class BlinkRunnable implements Runnable {
+		@Override
+		public void run() {
+			try {
+				while (blinkCursor) {
+					blinking = !blinking;
+					JConsole.this.repaint();
+					Thread.sleep(300L);
+				}
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}		
+	}
+	
 
 }
