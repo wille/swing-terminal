525a4c86ac260488ffe2bd5adc4af08c51dd0c34
redpois0n <red@cmail.nu>
2015-02-22 11:03:26 +0100

    Blinking completed

--- src/com/redpois0n/console/JConsole.java
+++ src/com/redpois0n/console/JConsole.java
@@ -1,11 +1,18 @@
 package com.redpois0n.console;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 
 import javax.swing.JComponent;
 
 @SuppressWarnings("serial")
 public class JConsole extends JComponent {
+	
+	private final Font[] fonts;
+	private final Color[] foreground;
+	private final Color[] background;
+	private final char[] chars;
 	
 	private int columns;
 	private int rows;
@@ -14,21 +21,39 @@ public class JConsole extends JComponent {
 	private int charheight;
 	
 	private boolean blinking;
-	private boolean blinkCursor;
+	private boolean blinkcursor;
+	
+	private int cursorrow;
+	private int cursorcolumn;
 		
 	public JConsole() {
 		this.columns = 80;
 		this.rows = 30;
 		
+		this.fonts = new Font[getTotal()];
+		this.foreground = new Color[getTotal()];
+		this.background = new Color[getTotal()];
+		this.chars = new char[getTotal()];
+
 		this.charwidth = 8;
 		this.charheight = 12;
 		
-		this.blinkCursor = true;
+		toggleBlink();
 	}
 
 	@Override
 	public void paintComponent(Graphics g) {
+		g.setColor(Color.black);
 		g.fillRect(0, 0, getRealX(columns), getRealY(rows));
+		
+		if (blinking) {
+			g.setColor(Color.white);
+			g.fillRect(getRealX(cursorrow), getRealY(cursorcolumn), charwidth, charheight);
+		}
+	}
+	
+	public int getTotal() {
+		return columns + rows * columns;
 	}
 	
 	public int getRealX(int x) {
@@ -40,9 +65,9 @@ public class JConsole extends JComponent {
 	}
 	
 	public void toggleBlink() {
-		blinkCursor = !blinkCursor;
+		blinkcursor = !blinkcursor;
 		
-		if (blinkCursor) {
+		if (blinkcursor) {
 			new Thread(new BlinkRunnable()).start();
 		}
 	}
@@ -51,7 +76,7 @@ public class JConsole extends JComponent {
 		@Override
 		public void run() {
 			try {
-				while (blinkCursor) {
+				while (blinkcursor) {
 					blinking = !blinking;
 					JConsole.this.repaint();
 					Thread.sleep(300L);
