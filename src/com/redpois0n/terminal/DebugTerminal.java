68530f08765e8011c9f12e2b784bc6d4bfea24ac
redpois0n <red@cmail.nu>
2015-02-22 13:22:47 +0100

    Adds JTerminal to scroll panel

--- src/com/redpois0n/terminal/DebugTerminal.java
+++ src/com/redpois0n/terminal/DebugTerminal.java
@@ -1,16 +1,20 @@
 package com.redpois0n.terminal;
 
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
 
 public class DebugTerminal {
 
 	public static void main(String[] args) {
 		JTerminal console = new JTerminal();
 		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setViewportView(console);
+		
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.addKeyListener(console.getKeyListener());
-		frame.add(console);
+		frame.add(scrollPane);
 		frame.setSize(650, 400);
 		frame.setVisible(true);	
 	}
