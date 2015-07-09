7330e010d3e879bb85a14fb122a2d5d9b39a6684
redpois0n <red@cmail.nu>
2015-07-09 13:20:30 +0200

    Color work (test with git log --graph --color

--- src/com/redpois0n/terminal/JTerminal.java
+++ src/com/redpois0n/terminal/JTerminal.java
@@ -5,9 +5,15 @@ import java.awt.Font;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.swing.JTextPane;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyleContext;
 import javax.swing.text.StyledDocument;
 
 @SuppressWarnings("serial")
@@ -21,8 +27,32 @@ public class JTerminal extends JTextPane {
 	public static final char ESCAPE = 27;
 	public static final String UNIX_CLEAR = ESCAPE + "[H" + ESCAPE + "[J";
 
+	public static final Map<String, Color> COLORS = new HashMap<String, Color>();
+
 	static {		
 		DEFAULT_FONT = new Font("monospaced", Font.PLAIN, 14);
+		COLORS.put("30", Color.black);
+		COLORS.put("31", Color.red.darker());
+		COLORS.put("32", Color.green.darker());
+		COLORS.put("33", Color.yellow.darker());
+		COLORS.put("34", Color.blue);
+		COLORS.put("35", Color.magenta.darker());
+		COLORS.put("36", Color.cyan.darker());
+		COLORS.put("37", Color.white);
+	}
+	
+	public static Color getColor(String s) {
+		 Color color = DEFAULT_FOREGROUND;
+		 
+		 if (s.endsWith("m")) {
+			 s = s.substring(0, s.length() - 1);
+		 }
+		 
+		 if (COLORS.containsKey(s)) {
+			 color = COLORS.get(s);
+		 }
+	
+		 return color;
 	}
 	
 	private List<InputListener> inputListeners = new ArrayList<InputListener>();
@@ -71,10 +101,63 @@ public class JTerminal extends JTextPane {
 		//append(c);
 	}
 	
-	public synchronized void append(String s) {		
-		setText(super.getText() + s);
-		last = doc.getLength();
+	public synchronized void append(String s) {
+        last = doc.getLength();
+
+		Color foreground = DEFAULT_FOREGROUND;
+		Color background = DEFAULT_BACKGROUND;
+		
+		String s1 = "";
+		
+        for (int cp = 0; cp < s.toCharArray().length; cp++) {
+            char c = s.charAt(cp);
+            
+            if (c == ESCAPE) {
+            	append(foreground, s1);
+                char next = s.charAt(cp + 1);
+                
+                if (next == '[') {
+                	s1 = "";
+                	cp++;
+                	while ((c = s.charAt(++cp)) != 'm') {
+                		s1 += c;
+                	}
+                	s1 += 'm';
+                    
+                    if (s1.equals("0m") || s1.equals("m")) {                        
+                        foreground = DEFAULT_FOREGROUND;
+                    } else if (s1.length() > 0) {
+                        System.out.println(s1);
+                        
+                        foreground = getColor(s1);
+                    }
+                    
+                    s1 = "";
+                    continue;
+                }
+            }
+            
+            s1 += c;
+        }
+        
+        if (s1.length() > 0) {
+        	append(foreground, s1);
+        }
+        
+        last = doc.getLength();
+
+        setCursorInEnd();
+        
+	}
+	
+	 public void append(Color foreground, String s) { 
+		StyleContext sc = StyleContext.getDefaultStyleContext();
+		AttributeSet aset = sc.addAttribute(SimpleAttributeSet.EMPTY, StyleConstants.Foreground, foreground);
+
 		setCursorInEnd();
+		
+		setCharacterAttributes(aset, false);
+		replaceSelection(s);
 	}
 	
 	public void setCursorInEnd() {
